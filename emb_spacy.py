645c96ed737bcc8c7f163c0d097fdfd57ec66851
Camille.Autain <cautain@DELL-CAM2>
2024-04-10 11:32:37 +0200

    add how to download spacy.py

--- emb_spacy.py
+++ emb_spacy.py
@@ -7,3 +7,8 @@ nlp = spacy.load("en_core_web_md")
 def get_embedding(mot):
     embedding = nlp.vocab[mot].vector
     return embedding
+
+
+#####  POUR POUVOIR UTILISER SPACY IL FAUT D'ABORD L'INSTALLER  #####
+# $ pip install spacy
+# $ python3 -m spacy download en_core_web_md
